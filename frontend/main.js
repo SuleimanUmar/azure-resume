40f78059901e639efe09a43db5d6215821a2c48d
Suleiman Umar <38038048+Traserlot@users.noreply.github.com>
2024-03-24 10:39:58 +0100

    Created CI/CD workflows

--- frontend/main.js
+++ frontend/main.js
@@ -4,8 +4,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 // const localApi = 'http://localhost:7071/api/GetResumeCounter';
-const functionApiUrl = 'http://localhost:7071/api/GetResumeCounter'; 
-// const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter'; 
+const functionApiUrl = 'https://getresumecountersumar.azurewebsites.net/api/GetResumeCounter?code=IQsHW3hOlLHd47wipilP_GpBwUPSmCjbg_Y4XBNQ7B2AAzFu9DHecQ=='; 
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter'; 
 
 const getVisitCount = () => {
     let count = 30;
